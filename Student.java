ea82719e7376690cb303d1f1aada764f236520f8
Bogdan Velichkovski <bogdance98@gmail.com>
2020-04-12 22:03:05 +0200

    student  class  finished
    student  class  finished des

--- Student.java
+++ Student.java
@@ -4,16 +4,50 @@ class Student {
 	String lastName;
 
 	List<Integer> labPoints=new ArrayList<Integer>();
-	
-	//TODO constructor
 
-	//TODO seters & getters
+ public Student(String index, String firstName, String lastName, List<Integer> labPoints) {
+        this.index = index;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.labPoints = labPoints;
+    }
 
-	public double getAverage() {
-		//TODO
-	}
+    public String getIndex() {
+        return index;
+    }
 
-	public boolean hasSignature() {
-		//TODO
-	}
+    public void setIndex(String index) {
+        this.index = index;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public double getAverage() {
+        int vkupno=0;
+        for(int i=0;i<this.labPoints.size();i++){
+            vkupno=vkupno+this.labPoints.get(i); }
+        return vkupno/this.labPoints.size();
+    }
+    public boolean hasSignature() {
+        if(labPoints.size()>8){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
 }
